26673ebb8c578bee7ffaece8126e226517a70296
Geoff <gschroeder@live.ca>
2020-09-26 03:38:39 -0700

    Re-organize local config files for quick update to docker image. Create Docker setup script in PowerShell. ToDo: - Create Docker setup file in bash - Add 'Read Info' for docker tag value - Create bash file for 'local' setup if not wanting to use docker. - Replace 'dockerimageconfigs' to proper inline updates within Dockerfile

---
+++ Dockersetup.ps1
@@ -0,0 +1,34 @@
+#!/usr/bin/pwsh
+
+#region Settings
+    $imageNameBase = "plexmovieposterdisplay"
+    # $tag = "latest"
+    $tag = "2.0.c"
+    $imageNameFull = "$($imageNameBase):$($tag)"
+
+    $containerName = "plexmovieposterdisplayinstance"
+#endregion
+
+#region Remove Image
+    docker image rm $($imageName)
+#endregion
+
+#region Build Image
+    Write-Output "Build Image Name (Full): $($imageNameFull)"
+    docker build -t $($imageNameFull) . 
+#endregion
+
+#region Stop and remove running container
+    # docker stop $($containerName)
+    # docker rm $($containerName)
+
+    docker rm -f $($containerName)
+#endregion
+
+#region Run container
+    docker run -d -p 80:80 --name $($containerName) $($imageNameFull)
+    # docker run -it --name $($containerName) $($imageNameBase)
+    # docker run -it -p 81:80 --name $($containerName) $($imageNameBase)
+
+    # docker-compose up
+#endregion
